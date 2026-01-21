cdfcfa26afb995cad373b4988bdeb8223c2212b5
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-01-21 03:59:38 -0600

    kinda finished up locatio ntypes

--- src/RWOS.Model/LocationTypes/LocationTypesModel.vb
+++ src/RWOS.Model/LocationTypes/LocationTypesModel.vb
@@ -1,4 +1,5 @@
-﻿Imports RWOS.Data
+﻿Imports System.Text.Json
+Imports RWOS.Data
 
 Friend Class LocationTypesModel
     Implements ILocationTypesModel
@@ -9,12 +10,31 @@ Friend Class LocationTypesModel
         Me.data = data
     End Sub
 
+    Public ReadOnly Property Names As IEnumerable(Of String) Implements ILocationTypesModel.Names
+        Get
+            Return data.LocationTypes.Keys
+        End Get
+    End Property
+
+    Public Sub Import(data As String) Implements ILocationTypesModel.Import
+        Me.data.LocationTypes = JsonSerializer.Deserialize(Of Dictionary(Of String, LocationTypeData))(data)
+    End Sub
+
     Public Function Create(locationTypeName As String, imageName As String) As ILocationTypeModel Implements ILocationTypesModel.Create
-        data.LocationTypes(locationTypeName) = New LocationTypeData With {.imageName = imageName}
+        data.LocationTypes(locationTypeName) = New LocationTypeData With {.ImageName = imageName}
         Return GetLocationType(locationTypeName)
     End Function
 
     Public Function GetLocationType(locationTypeName As String) As ILocationTypeModel Implements ILocationTypesModel.GetLocationType
         Return New LocationTypeModel(data, locationTypeName)
+    End Function
+
+    Public Function ImportLocationType(locationTypeName As String, data As String) As ILocationTypeModel Implements ILocationTypesModel.ImportLocationType
+        Me.data.LocationTypes(locationTypeName) = JsonSerializer.Deserialize(Of LocationTypeData)(data)
+        Return GetLocationType(locationTypeName)
+    End Function
+
+    Public Function Export() As String Implements ILocationTypesModel.Export
+        Return JsonSerializer.Serialize(data.LocationTypes)
     End Function
 End Class
--- src/RWOS.UI/State/Editor/LocationTypes/EditLocationTypesUI.vb
+++ src/RWOS.UI/State/Editor/LocationTypes/EditLocationTypesUI.vb
@@ -8,13 +8,62 @@ Friend Class EditLocationTypesUI
         MyBase.New(controls, model, "Location Types", CGAHue.CYAN, New PickerMenu(EditorMenuUI.Launch(controls, model)))
         _menu.AddChoice("Go Back", EditorMenuUI.Launch(controls, model))
         _menu.AddChoice("New Location Type", CreateLocationTypeUI.Launch(controls, model))
-        '_menu.AddChoice("Import Single Location Type...", ImportSingleLocationType(controls, model, Me))
-        '_menu.AddChoice("Export All...", ExportAllLocationTypes(controls, model, Me))
-        '_menu.AddChoice("Import All...", ImportAllLocationTypes(controls, model, Me))
-        'For Each locationTypeName In model.LocationTypes.Names
-        '    _menu.AddChoice(locationTypeName, EditLocationType(locationTypeName))
-        'Next
+        _menu.AddChoice("Import Single Location Type...", ImportSingleLocationType(controls, model, Me))
+        _menu.AddChoice("Export All...", ExportAllLocationTypes(controls, model, Me))
+        _menu.AddChoice("Import All...", ImportAllLocationTypes(controls, model, Me))
+        For Each locationTypeName In model.LocationTypes.Names
+            _menu.AddChoice(locationTypeName, EditLocationType(locationTypeName))
+        Next
     End Sub
+
+    Private Function ImportAllLocationTypes(controls As IHostControls, model As IWorldModel, ui As EditLocationTypesUI) As Func(Of IUI(Of CGAHue))
+        Return Function()
+                   Return TextEditUI.Launch(controls, model, "Import All Location Types From...", CGAHue.CYAN, String.Empty, HandleImportAll(controls, model), Function() ui).Invoke
+               End Function
+    End Function
+
+    Private Function HandleImportAll(controls As IHostControls, model As IWorldModel) As Func(Of String, IUI(Of CGAHue))
+        Return Function(filename)
+                   model.LocationTypes.Import(controls.Load(filename))
+                   Return EditLocationTypesUI.Launch(controls, model).Invoke
+               End Function
+    End Function
+
+    Private Function ExportAllLocationTypes(controls As IHostControls, model As IWorldModel, ui As EditLocationTypesUI) As Func(Of IUI(Of CGAHue))
+        Return Function()
+                   Return TextEditUI.Launch(controls, model, "Export All Location Types To...", CGAHue.CYAN, String.Empty, HandleExportAll(controls, model), Function() ui).Invoke
+               End Function
+    End Function
+
+    Private Function HandleExportAll(controls As IHostControls, model As IWorldModel) As Func(Of String, IUI(Of CGAHue))
+        Return Function(filename)
+                   controls.Save(filename, model.LocationTypes.Export())
+                   Return EditLocationTypesUI.Launch(controls, model).Invoke
+               End Function
+    End Function
+
+    Private Shared Function ImportSingleLocationType(controls As IHostControls, model As IWorldModel, ui As IUI(Of CGAHue)) As Func(Of IUI(Of CGAHue))
+        Return Function()
+                   Return TextEditUI.Launch(controls, model, "Import Location Type From...", CGAHue.CYAN, String.Empty, HandleImport(controls, model, ui), Function() ui).Invoke
+               End Function
+    End Function
+
+    Private Shared Function HandleImport(controls As IHostControls, model As IWorldModel, ui As IUI(Of CGAHue)) As Func(Of String, IUI(Of CGAHue))
+        Return Function(filename)
+                   Return TextEditUI.Launch(controls, model, "Imported Location Type Name...", CGAHue.CYAN, String.Empty, HandleImport2(controls, model, filename), Function() ui).Invoke
+               End Function
+    End Function
+
+    Private Shared Function HandleImport2(controls As IHostControls, model As IWorldModel, filename As String) As Func(Of String, IUI(Of CGAHue))
+        Return Function(locationTypeName)
+                   model.LocationTypes.ImportLocationType(locationTypeName, controls.Load(filename))
+                   Return EditLocationTypeUI.Launch(controls, model, locationTypeName).Invoke
+               End Function
+    End Function
+
+    Private Function EditLocationType(locationTypeName As String) As Func(Of IUI(Of CGAHue))
+        Return Function() EditLocationTypeUI.Launch(Controls, Model, locationTypeName).Invoke
+    End Function
 
     Protected Overrides ReadOnly Property font As IFont
         Get
